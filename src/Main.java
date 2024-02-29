b11daeabf00efa937936fc9a693550946c2913f9
Ян <peroff007@gmail.com>
2024-02-29 20:53:13 +0300

    Прибавили в переменные 4

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,20 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-
+        System.out.println("Объявляем переменные:");
         System.out.println("Переменная dog = "+ dog +".");
         System.out.println("Переменная cat = "+ cat +".");
         System.out.println("Переменная paper = "+ paper +".");
+        System.out.println(" ");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("К переменным прибавлено 4:");
+        System.out.println("Переменная dog = "+ dog +".");
+        System.out.println("Переменная cat = "+ cat +".");
+        System.out.println("Переменная paper = "+ paper +".");
+        System.out.println(" ");
+
 
     }
 }
