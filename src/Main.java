9c22917886fc65f3e168c86a35412992c7b47995
Ян <peroff007@gmail.com>
2024-02-29 21:11:26 +0300

    Задача 2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         System.out.println("Переменная cat = "+ cat +".");
         System.out.println("Переменная paper = "+ paper +".");
         System.out.println(" ");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("К переменным прибавлено 4:");
+        System.out.println("Переменная dog = "+ dog +".");
+        System.out.println("Переменная cat = "+ cat +".");
+        System.out.println("Переменная paper = "+ paper +".");
+        System.out.println(" ");
 
     }
 }
