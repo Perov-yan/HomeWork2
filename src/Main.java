af98f6aecb4c8a191b1a45ef2a12108603723fe9
Ян <peroff007@gmail.com>
2024-02-29 21:06:14 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("Объявляем переменные:");
-        System.out.println("Переменная dog = "+ dog +".");
-        System.out.println("Переменная cat = "+ cat +".");
-        System.out.println("Переменная paper = "+ paper +".");
-        System.out.println(" ");
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println("К переменным прибавлено 4:");
-        System.out.println("Переменная dog = "+ dog +".");
-        System.out.println("Переменная cat = "+ cat +".");
-        System.out.println("Переменная paper = "+ paper +".");
-        System.out.println(" ");
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println("Из переменных вычтены значения:");
-        System.out.println("Из dog - 3.5 = "+ dog +".");
-        System.out.println("Из cat - 1.6 = "+ cat +".");
-        System.out.println("Из paper - 7639 = "+ paper +".");
-        System.out.println(" ");
 
     }
 }
