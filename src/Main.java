bc3cf475b24c235a5935cc4fb672d9fbf3c2ff84
Ян <peroff007@gmail.com>
2024-02-29 21:10:13 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("Объявляем переменные:");
-        System.out.println("Переменная dog = "+ dog +".");
-        System.out.println("Переменная cat = "+ cat +".");
-        System.out.println("Переменная paper = "+ paper +".");
-        System.out.println(" ");
 
     }
 }
